ad720cada47c43b53cad254ef7ca5eaf5f5a5d19
Luis Sotomayor <luis.sotomayor@gmail.com>
2016-01-20 22:49:13 -0800

    Added function examples

--- src/helloworld.js
+++ src/helloworld.js
@@ -1,2 +1,13 @@
 var f = x => x + 2;
 f(34);
+
+
+function sayHello () {
+    let world = 'world';
+    let foo  = 'bar';
+    return `Hello ${world} ${foo}`;
+}
+
+function somethingElse(params) {
+    return 'foo';
+}
